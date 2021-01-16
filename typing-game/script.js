4e0c8003ea86f9a8bc602d71c0d47150cffaa2c4
Marie Serykh <marie.serykh@gmail.com>
2021-01-17 00:08:50 +0100

    Add list of quotes

---
+++ typing-game/script.js
@@ -0,0 +1,17 @@
+const quotes = [
+    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+    'There is nothing more deceptive than an obvious fact.',
+    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
+    'I never make exceptions. An exception disproves the rule.',
+    'What one man can invent another can discover.',
+    'Nothing clears up a case so much as stating it to another person.',
+    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+    'You see, but you do not observe.',
+    'Mediocrity knows nothing higher than itself; but talent instantly recognizes genius',
+];
+let words = [];
+let wordIndex = 0;
+let startTime = Date.now();
+const quoteValue = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const typeValueElement = document.getElementById('typed-value');
