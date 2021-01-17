8afe2c59579d263744a323e194e4a95fd1c7eb9a
Marie Serykh <marie.serykh@gmail.com>
2021-01-17 23:20:04 +0100

    * Disable the input event listener on completion * Disable the textbox when the player completes the quote * Display a modal dialog box with the success message

--- typing-game/script.js
+++ typing-game/script.js
@@ -20,6 +20,9 @@ const typedValueElement = document.getElementById('typed-value');
 const startButton = document.getElementById('start');
 
 startButton.addEventListener('click', function() {
+  typedValueElement.addEventListener('input', checkTyping, true);
+  typedValueElement.disabled = false;
+
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[quoteIndex];
 
@@ -40,37 +43,47 @@ startButton.addEventListener('click', function() {
   startTime = new Date().getTime();
 });
 
-typedValueElement.addEventListener('input', (e) => {
-  const currentWord = words[wordIndex];
-  const typedValue = typedValueElement.value;
+typedValueElement.addEventListener('input', checkTyping, true);
 
-  if (typedValue === currentWord && wordIndex === words.length - 1) {
-    
-    const elapsedTime = new Date().getTime() - startTime;
-    const message = `🎉You finished in ${
-      elapsedTime / 1000
-    } seconds🎉`;
-    typedValueElement.innerText = '';
-    messageElement.innerText = message;
-
-  } 
-  else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
-
-    typedValueElement.value = '';
-    wordIndex++;
-
-    for (const wordElement of quoteElement.childNodes) {
-      wordElement.className = '';
+function checkTyping() {
+    const currentWord = words[wordIndex];
+    const typedValue = typedValueElement.value;
+  
+    if (typedValue === currentWord && wordIndex === words.length - 1) {
+      
+      const elapsedTime = new Date().getTime() - startTime;
+      const message = `🎉You finished in ${Math.floor(elapsedTime / 1000)} seconds🎉`;
+          
+      //messageElement.innerText = message;
+      typedValueElement.value = '';
+      typedValueElement.disabled = true;
+      typedValueElement.removeEventListener('input', checkTyping, true);
+      
+      setTimeout(function() {
+        alert(message)
+    }, 500);
+      
+    } 
+    else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+  
+      typedValueElement.value = '';
+      ++wordIndex;
+  
+      for (const wordElement of quoteElement.childNodes) {
+        wordElement.className = '';
+      }
+      
+      quoteElement.childNodes[wordIndex].className = 'highlight';
+    } 
+    else if (currentWord.startsWith(typedValue)) {
+      
+      typedValueElement.className = '';
+    } 
+    else {
+      
+      typedValueElement.className = 'error';
     }
-    
-    quoteElement.childNodes[wordIndex].className = 'highlight';
-  } 
-  else if (currentWord.startsWith(typedValue)) {
-    
-    typedValueElement.className = '';
-  } 
-  else {
-    
-    typedValueElement.className = 'error';
   }
-});
+
+
+
