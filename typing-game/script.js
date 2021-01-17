08abefe22b88564a1fe3cef0245dce5fea68ef1f
Marie Serykh <marie.serykh@gmail.com>
2021-01-17 15:56:04 +0100

    Fix typos and changee double quotes to single quotes

--- typing-game/script.js
+++ typing-game/script.js
@@ -1,67 +1,76 @@
 const quotes = [
-  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth.",
-  "There is nothing more deceptive than an obvious fact.",
-  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.",
-  "I never make exceptions. An exception disproves the rule.",
-  "What one man can invent another can discover.",
-  "Nothing clears up a case so much as stating it to another person.",
-  "Education never ends, Watson. It is a series of lessons, with the greatest for the last.",
-  "You see, but you do not observe.",
-  "Mediocrity knows nothing higher than itself; but talent instantly recognizes genius.",
+  "When you have eliminated the impossible, whatever remains, however improbable, must be the truth",
+  "There is nothing more deceptive than an obvious fact",
+  "I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation",
+  "I never make exceptions. An exception disproves the rule",
+  "What one man can invent another can discover",
+  "Nothing clears up a case so much as stating it to another person",
+  "Education never ends, Watson. It is a series of lessons, with the greatest for the last",
+  "You see, but you do not observe",
+  "Mediocrity knows nothing higher than itself; but talent instantly recognizes genius",
 ];
+
 let words = [];
 let wordIndex = 0;
 let startTime = Date.now();
-const quoteElement = document.getElementById("quote");
-const messageElement = document.getElementById("message");
-const typeValueElement = document.getElementById("typed-value");
-const startButton = document.getElementById("start");
 
-startButton.addEventListener("click", () => {
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const typedValueElement = document.getElementById('typed-value');
+const startButton = document.getElementById('start');
+
+startButton.addEventListener('click', function() {
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[quoteIndex];
 
-  words = quote.split(" ");
-
+  words = quote.split(' ');
   wordIndex = 0;
 
   const spanWords = words.map(function (word) {
     return `<span>${word} </span>`;
   });
 
-  quoteElement.innerHTML = spanWords.join("");
+  quoteElement.innerHTML = spanWords.join('');
   quoteElement.childNodes[0].className = "highlight";
 
-  messageElement.innerText = "";
+  messageElement.innerText = '';
 
-  typedValueElement.value = "";
+  typedValueElement.value = '';
+  typedValueElement.focus();
   startTime = new Date().getTime();
 });
 
-typeValueElement.addEventListener("input", () => {
+typedValueElement.addEventListener('input', (e) => {
   const currentWord = words[wordIndex];
-  const typedValue = typeValueElement.value;
-  const elapsedTime = new Date() - startTime;
+  const typedValue = typedValueElement.value;
 
   if (typedValue === currentWord && wordIndex === words.length - 1) {
-    const message = `CONGRATULATIONS! You finished in ${
+    
+    const elapsedTime = new Date().getTime() - startTime;
+    const message = `🎉You finished in ${
       elapsedTime / 1000
-    } seconds.`;
-    messageElement.innerHTML = message;
+    } seconds🎉`;
+    typedValueElement.innerText = '';
+    messageElement.innerText = message;
+
   } 
-  else if (typedValue.endsWith(" ") && typedValue.trim === currentWord) {
-    typedValueElement.value = "";
+  else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+
+    typedValueElement.value = '';
     wordIndex++;
 
     for (const wordElement of quoteElement.childNodes) {
-      wordElement.className = "";
+      wordElement.className = '';
     }
+    
     quoteElement.childNodes[wordIndex].className = 'highlight';
-  }
-  else if (currentWordstartsWith(typedValue)) {
+  } 
+  else if (currentWord.startsWith(typedValue)) {
+    
     typedValueElement.className = '';
-  }
+  } 
   else {
-      typedValueElement.className = 'error';
+    
+    typedValueElement.className = 'error';
   }
 });
